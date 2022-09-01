5ed120c7bb96dda969cb7f1cef19852b6fd775cb
iamkellpham <Bich-Tram.Pham01@student.csulb.edu>
2022-08-31 19:20:26 -0700

    Adding 4 main activities
     Main, Login, Suggestion, Account

---
+++ app/src/main/java/com/example/foodstabook/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.example.foodstabook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
+

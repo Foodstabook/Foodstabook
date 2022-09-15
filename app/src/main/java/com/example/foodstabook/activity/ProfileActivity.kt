f30723360e6513853455b80c1df8b608246178fd
Kevin Garcia <kevin.garciajulca@student.csulb.edu>
2022-09-14 23:02:43 -0700

    Update ProfileActivity.kt

--- app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
@@ -2,9 +2,11 @@ package com.example.foodstabook.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.foodstabook.R //Not used when we use binding
 import com.example.foodstabook.databinding.ActivityProfileBinding
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -24,6 +26,21 @@ class ProfileActivity : AppCompatActivity() {
         binding.buttonUserOne.setOnClickListener{
             val userName : String = binding.displayProfileName.text.toString()
             val userEmail : String = binding.displayProfileEmail.text.toString()
+
+            if(userName.isNotEmpty()){
+                readData(userName)
+            }else{
+                Toast.makeText(this,"USERNAME ERROR",Toast.LENGTH_SHORT).show()
+            }
+
+
         }
+    }
+
+    private fun readData(userName: String) {
+
+        database = FirebaseDatabase.getInstance().getReference("users")
+        database.child(userName).get().addOnSuccessListener {  }
+
     }
 }

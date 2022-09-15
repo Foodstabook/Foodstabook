419a8c84c880137fc16add25aca81230fc1a0423
Kevin Garcia <kevin.garciajulca@student.csulb.edu>
2022-09-14 18:14:47 -0700

    update

--- app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
@@ -2,11 +2,28 @@ package com.example.foodstabook.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.foodstabook.R
+import com.example.foodstabook.R //Not used when we use binding
+import com.example.foodstabook.databinding.ActivityProfileBinding
+import com.google.firebase.database.DatabaseReference
 
 class ProfileActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityProfileBinding
+    private lateinit var database : DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
+
+
+        binding.buttonUserOne.setOnClickListener{
+            val userName : String = binding.displayProfileName.text.toString()
+            val userEmail : String = binding.displayProfileEmail.text.toString()
+        }
     }
 }

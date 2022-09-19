4be3b8cd957bd1ed5e0e504ef69c1e52d9b7057d
iamkellpham <Bich-Tram.Pham01@student.csulb.edu>
2022-09-19 16:00:36 -0700

    Forgot password navigation from main

--- app/src/main/java/com/example/foodstabook/MainActivity.kt
+++ app/src/main/java/com/example/foodstabook/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.foodstabook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.example.foodstabook.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.example.foodstabook.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        binding.tvResetPassword.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

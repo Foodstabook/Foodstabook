7c02048b2a26a54e870749df0a48c6151a694ff7
Kevin Garcia <kevin.garciajulca@student.csulb.edu>
2022-09-19 19:24:03 -0700

    fixed errors

--- app/src/main/java/com/example/foodstabook/ResetPassword.kt
+++ app/src/main/java/com/example/foodstabook/ResetPassword.kt
@@ -9,7 +9,7 @@ import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.foodstabook.databinding.ResetPasswordBinding
+import com.example.foodstabook.databinding.ActivityResetPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -21,12 +21,12 @@ class ResetPassword : AppCompatActivity() {
     // create instance of the ActivityMainBinding,
     // as we have only one layout activity_main.xml
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ResetPasswordBinding
+    private lateinit var binding: ActivityResetPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
         super.onCreate(savedInstanceState)
-        binding = ResetPasswordBinding.inflate(layoutInflater);
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater);
         setContentView(binding.root);
 
         binding.btnResetPassword.setOnClickListener {
--- app/src/main/java/com/example/foodstabook/activity/MainActivity.kt
+++ app/src/main/java/com/example/foodstabook/activity/MainActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.foodstabook.LoginActivity
-import com.example.foodstabook.R
+import com.example.foodstabook.*
+import com.example.foodstabook.MainActivity
+import com.example.foodstabook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         //To force change the title of the activity
         title = "Main Menu"
@@ -21,13 +25,46 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val button2:Button = findViewById(R.id.resetButton)
-        button2.setOnClickListener {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        goToLogin()
+        goToResetPassword()
+        goToFoodSuggestion()
+        goToUserAccount()
+        goHome()
+
+    }
+
+    private fun goToLogin(){
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
         }
+    }
 
+    private fun goToResetPassword(){
+        binding.resetPasswordButton.setOnClickListener{
+            val intent = Intent(this, ResetPassword::class.java)
+            startActivity(intent)
+        }
+    }
 
+    private fun goToFoodSuggestion(){
+        binding.foodSuggestionButton.setOnClickListener{
+            val intent = Intent(this, SuggestionMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
+    private fun goToUserAccount(){
+        binding.userAccountButton.setOnClickListener{
+            val intent = Intent(this, UserAccount::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun goHome(){
+        binding.logo.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

2a32cab0d86ff18a788aaa1f3f8e7cbeca750e43
Bich-Tram Pham <88569188+trampham1104@users.noreply.github.com>
2022-09-19 18:18:39 -0700

    update ResetPassword.kt on Retro-1

--- app/src/main/java/com/example/foodstabook/ResetPassword.kt
+++ app/src/main/java/com/example/foodstabook/ResetPassword.kt
@@ -1,24 +1,45 @@
 package com.example.foodstabook
 
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.foodstabook.databinding.ActivityResetPasswordBinding
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.foodstabook.databinding.ResetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
 
 class ResetPassword : AppCompatActivity() {
-    private lateinit var binding: ActivityResetPasswordBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        goHome()
-    }
 
-    private fun goHome(){
-        binding.logo.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+    // create instance of the ActivityMainBinding,
+    // as we have only one layout activity_main.xml
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ResetPasswordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+        super.onCreate(savedInstanceState)
+        binding = ResetPasswordBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        binding.btnResetPassword.setOnClickListener {
+            val emailAddress = binding.edtResetEmail.toString()
+
+            Firebase.auth.sendPasswordResetEmail(emailAddress).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Email sent!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        Log.d(TAG, "Email sent.")
+                    }
+                }
         }
     }
 }

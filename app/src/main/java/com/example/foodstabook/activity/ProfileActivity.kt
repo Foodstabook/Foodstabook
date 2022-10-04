e440e663f7ccf56ce5b0b80741265e5f47731db9
Kevin Garcia <kevin.garciajulca@student.csulb.edu>
2022-10-03 18:45:45 -0700

    added menu

--- app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/foodstabook/activity/ProfileActivity.kt
@@ -1,17 +1,26 @@
 package com.example.foodstabook.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.foodstabook.databinding.ActivityProfileBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
 
+    companion object{
+        private val REQUEST_SELECT_IMAGE_IN_ALBUM = 0
+        private val REQUEST_TAKE_PHOTO = 1
+    }
 
     private lateinit var binding : ActivityProfileBinding
     private lateinit var database : DatabaseReference
@@ -19,12 +28,12 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityProfileBinding.inflate(layoutInflater)
-//        setContentView(R.layout.activity_profile)
         setContentView(binding.root)
+
+//        setContentView(R.layout.activity_profile)
 
 
         binding.buttonSearch.setOnClickListener{
@@ -53,18 +62,23 @@ class ProfileActivity : AppCompatActivity() {
         goHome()
 
         binding.profileImage.setOnClickListener{
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            //startActivityForResult(gallery, pickImage)
+
+            //selectSourcePicture(buttonProfileLogOut)
+            //takePhoto()
+            selectImageInAlbum()
         }
 
     }
 
+    //Pulls information from a database with the UID
     private fun readData(uid: String) {
 
         database = FirebaseDatabase.getInstance().getReference("users")
         database.child(uid).get().addOnSuccessListener {
 
             if(it.exists()){
+
+                Toast.makeText(this,"User found!",Toast.LENGTH_SHORT).show()
 
                 val firstName = it.child("firstname").value
                 val lastName = it.child("lastname").value
@@ -73,28 +87,68 @@ class ProfileActivity : AppCompatActivity() {
                 val email = it.child("email").value
                 val age = it.child("age").value
 
-                Toast.makeText(this,"Successfully Read",Toast.LENGTH_SHORT).show()
-
                 binding.displayProfileName.text = name
                 binding.displayProfileId.text = id.toString()
                 binding.displayProfileEmail.text = email.toString()
                 binding.displayProfileAge.text = age.toString()
 
-
-
-            }else{
-                Toast.makeText(this,"User don't exist",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Info about user not found",Toast.LENGTH_SHORT).show()
             }
 
+
         }.addOnFailureListener{
-            Toast.makeText(this,"Fail",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Fail to get Data",Toast.LENGTH_SHORT).show()
         }
     }
 
+    //Moves the activity to another
     private fun goHome(){
         binding.logo.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+
+    // Takes a new photo
+    fun takePhoto() {
+        val intent1 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent1.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent1, REQUEST_TAKE_PHOTO)
+        }
+    }
+
+    //Select an Image from the album
+    fun selectImageInAlbum() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+        }
+    }
+
+
+    //
+    fun selectSourcePicture(view: View) {
+
+        val cancelButtonClick = { dialog: DialogInterface, which: Int ->
+            Toast.makeText(applicationContext, android.R.string.cancel, Toast.LENGTH_SHORT).show()
+        }
+
+        val items = arrayOf("Camera", "Gallery")
+        val builder = AlertDialog.Builder(this)
+        with(builder)
+        {
+            setTitle("Get an Image from :")
+            setItems(items) { dialog, which ->
+                Toast.makeText(applicationContext, items[which] + " is clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            setPositiveButton("Cancel", cancelButtonClick)
+            show()
         }
     }
 
